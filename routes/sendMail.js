ded4c11517985696c02e66d838463d4877c5fba0
lavkumarsingh <lavksingh68495@gmail.com>
2022-01-09 03:20:10 +0530

    'script'

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 // routes
-const route = require('../controller/controller')
+const route = require('../controller/mailController')
 
 router.post('/', route.index)
 router.post('/send-mail', route.sendMail)
